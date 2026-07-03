e258ce9f40472d5cb24f8c9e2c4ef067f5998d3e
Nicolas_GG <nicolasgg2020@gmail.com>
2026-07-03 18:14:17 -0500

    Añadiendo funcionalidad basica de login

---
+++ projects/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index_view, name='index'),
+    path('login/', views.login_view, name='login'),
+    path('home/', views.home_view, name='home')
+]
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 # Create your views here.
+def login_view(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    else:
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username = username, password = password)
+        if user != None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'login.html', {'error': 'Usuario o contraseña incorrectos'})
+
+def home_view(request):
+    return render(request, 'home.html')
+
+def index_view(request):
+    return render(request, 'index.html')
