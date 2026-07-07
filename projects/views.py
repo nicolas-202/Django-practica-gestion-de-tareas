85189f0d6ca4cacb40b12ce250a070b291df59e3
Nicolas_GG <nicolasgg2020@gmail.com>
2026-07-06 23:03:29 -0500

    Añadiendo navBar y funcionalidad logout

--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def login_view(request):
     if request.method == 'GET':
-        return render(request, 'login.html')
+        if request.user.is_authenticated:
+            return redirect('home')
+        else:
+            return render(request, 'login.html')
     else:
         username = request.POST["username"]
         password = request.POST["password"]
@@ -15,10 +18,17 @@ def login_view(request):
         else:
             return render(request, 'login.html', {'error': 'Usuario o contraseña incorrectos'})
 
+def logout_view(request):
+    logout(request)
+    return redirect('index')
+
 def register_view(request):
     if request.method == 'GET':
-        form = UserCreationForm()
-        return render(request, 'register.html', {'register_form': form})
+        if request.user.is_authenticated:
+            return redirect('home')
+        else:
+            form = UserCreationForm()
+            return render(request, 'register.html', {'register_form': form})
     else:
         form = UserCreationForm(request.POST)
         if form.is_valid():
